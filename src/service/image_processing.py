bd4c6f81dad65373726edb606b74ff227640f033
ilya_groza <ilya.groza13@gmail.com>
2025-04-11 14:35:45 +0400

    feat: enhance image processing with progress bar and multithreading support

--- src/service/image_processing.py
+++ src/service/image_processing.py
@@ -1,4 +1,3 @@
-import asyncio
 import logging
 from concurrent.futures import ThreadPoolExecutor
 
@@ -23,7 +22,29 @@ class ImageProcessing:
             logging.error("Ошибка обработки изображения '%s': %s", image_path, e)
             return image_path, None
 
-    def process_images(self, image_paths: list[str]) -> dict[str, imagehash.ImageHash]:
+    def calculate_hashes_single_thread(self, image_paths: list[str]) -> dict[str, imagehash.ImageHash]:
+        """
+         Синхронная обработка изображений.
+         Возвращает словарь, где ключ — путь к изображению, а значение — вычисленный хэш.
+
+         :param image_paths: Список путей к изображениям
+         :return: Словарь {путь_к_изображению: хэш}
+         """
+
+        logging.info(f"Начато вычисление хэшей")
+
+        # Прогрессбар для отображения статуса обработки
+        with click.progressbar(image_paths, length=len(image_paths), show_eta=True,
+                               label='Обработка изображений') as bar:
+            hash_images = []
+            for image_path in bar:
+                result = self.calculate_image_hash(image_path)
+                hash_images.append(result)
+
+        # Формирование словаря из успешных результатов
+        return {path: hash_value for path, hash_value in hash_images if hash_value is not None}
+
+    def calculate_hashes_multithreaded(self, image_paths: list[str]) -> dict[str, imagehash.ImageHash]:
         """
         Синхронная обработка изображений.
         Возвращает словарь, где ключ — путь к изображению, а значение — вычисленный хэш.
@@ -32,7 +53,7 @@ class ImageProcessing:
         :return: Словарь {путь_к_изображению: хэш}
         """
 
-        logging.info(f"Начато вычисление хэшей для {len(image_paths)} изображений")
+        logging.info(f"Начато вычисление хэшей")
 
         # Оптимальное количество потоков
         max_workers = get_optimized_thread_count()
@@ -47,31 +68,3 @@ class ImageProcessing:
 
         # Формирование словаря из успешных результатов
         return {path: hash_value for path, hash_value in hash_images if hash_value is not None}
-
-    async def process_images_async(self, image_paths: list[str]) -> dict[str, imagehash.ImageHash]:
-
-        """
-        Асинхронная обработка изображений с вычислением хэшей.
-        Возвращает словарь {путь к изображению: хэш}, исключая неуспешно обработанные.
-
-        :param image_paths: список путей к изображениям
-        :return: словарь {путь_к_изображению: хэш}
-        """
-        logging.info(f"Начато вычисление хэшей для {len(image_paths)} изображений")
-
-        # Оптимальное количество потоков
-        max_workers = get_optimized_thread_count()
-
-        # Асинхронная обработка изображений с помощью пула потоков
-        async def handle_image_in_pool(image_path):
-            loop = asyncio.get_running_loop()
-            return await loop.run_in_executor(pool, self.calculate_image_hash, image_path)
-
-        # Создание пула потоков
-        with ThreadPoolExecutor(max_workers=max_workers) as pool:
-            # Обработка изображений и сбор результатов
-            tasks = [handle_image_in_pool(path) for path in image_paths]
-            results = await asyncio.gather(*tasks)
-
-        # Фильтр успешных хэшей
-        return {path: hash_value for path, hash_value in results if hash_value is not None}
