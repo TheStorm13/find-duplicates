1c1e5afe79dd6750673408616b454cc79498b1b6
ilya_groza <ilya.groza13@gmail.com>
2025-04-11 13:50:17 +0400

    feat: implement file management for duplicates with metadata handling

---
+++ src/service/files/file_manager.py
@@ -0,0 +1,106 @@
+import os
+import logging
+from config import METADATA_FILE_NAME, DUPLICATE_FOLDER
+from src.service.files.metadata_manager import MetadataManager
+
+
+class FileManager:
+    def create_duplicate_dir(self, base_directory: str) -> str:
+        """
+        Создаёт папку для дубликатов, если она ещё не существует.
+
+        :param base_directory: Базовая директория.
+        :return: Путь к созданной или уже существующей директории.
+        """
+
+        duplicate_dir = os.path.join(base_directory, DUPLICATE_FOLDER)
+        os.makedirs(duplicate_dir, exist_ok=True)
+        logging.info(f"Создана директория для дубликатов: {duplicate_dir}")
+        return duplicate_dir
+
+    def move_file(self, src_path: str, dst_dir: str) -> None:
+        """
+        Перемещает файл из src_path в указанную директорию.
+
+        :param src_path: Путь к исходному файлу.
+        :param dst_dir: Путь к директории назначения.
+        """
+
+        try:
+            filename = os.path.basename(src_path)
+            dst_path = os.path.join(dst_dir, filename)
+            os.replace(src_path, dst_path)
+            logging.debug(f"Файл {src_path} перемещён в {dst_path}")
+        except Exception as e:
+            logging.error(f"Ошибка при перемещении файла {src_path} в {dst_dir}: {e}")
+
+    def move_duplicates(self, base_directory: str, duplicates: list[str]) -> None:
+        """
+        Перемещает дубликаты в указанную директорию.
+
+        :param duplicates: Словарь с группами дубликатов.
+        :param duplicate_dir: Директория для хранения дубликатов.
+        """
+        duplicate_dir = self.create_duplicate_dir(base_directory)
+
+        metadata_manager = MetadataManager(os.path.join(duplicate_dir, METADATA_FILE_NAME))
+        original_paths = {}
+
+        for duplicate_path in duplicates:
+            self.move_file(duplicate_path, duplicate_dir)
+            file_name = os.path.basename(duplicate_path)
+            original_paths[os.path.join(duplicate_dir, file_name) ] = duplicate_path
+        logging.info(f"Дубликаты успешно перемещены в директорию: {duplicate_dir}")
+
+        # Сохраняем информацию об исходных путях для возврата
+        metadata_manager.save_metadata(original_paths)
+
+    def return_file_to_original_location(self, current_path: str, original_path: str) -> None:
+        """
+        Возвращает дубликат на его исходное место.
+
+        :param current_path: Текущий путь файла в папке дубликатов.
+        :param original_path: Исходный путь файла.
+        """
+        try:
+            # Проверка, существует ли файл на оригинальном пути
+            if os.path.exists(original_path):
+                logging.warning(f"Файл {original_path} уже существует. Добавляю суффикс '_duplicate'.")
+                base, ext = os.path.splitext(original_path)
+                original_path = f"{base}_duplicate{ext}"
+
+            # Перемещаем файл обратно
+            os.replace(current_path, original_path)
+            logging.debug(f"Файл {current_path} возвращён в {original_path}")
+
+        except Exception as e:
+            logging.error(f"Ошибка при возврате файла {current_path} в {original_path}: {e}")
+
+    def return_duplicates(self, base_directory: str) -> None:
+        """
+        Возвращает все дубликаты из папки для дубликатов обратно в их исходные директории.
+
+        :param duplicates_folder: Папка, где хранятся дубликаты.
+        """
+        duplicate_dir = os.path.join(base_directory, "Duplicate")
+        metadata_manager = MetadataManager(os.path.join(duplicate_dir, METADATA_FILE_NAME))
+
+        # Проверяем, существуют ли метаданные
+        if not metadata_manager.metadata_exists():
+            logging.error(f"Файл метаданных не найден в директории {duplicate_dir}.")
+            return
+
+        # Загрузка метаданных
+        original_paths = metadata_manager.load_metadata()
+        logging.info("Будет возвращено %d файлов.>", len(original_paths))
+        # Возвращаем файлы
+        for current_path, original_path in original_paths.items():
+            if os.path.exists(current_path):
+                self.return_file_to_original_location(current_path, original_path)
+            else:
+                logging.warning(f"Файл {current_path} отсутствует в папке дубликатов. Пропускаю.")
+
+        logging.info("Все возможные дубликаты возвращены на исходные позиции.")
+
+        # Удаляем файл метаданных после возврата
+        metadata_manager.clean_metadata()
---
+++ src/service/files/metadata_manager.py
@@ -0,0 +1,68 @@
+import json
+import logging
+import os
+
+
+class MetadataManager:
+    def __init__(self, metadata_file: str):
+        """
+        Инициализация менеджера для работы с метаданными.
+
+        :param metadata_file: Путь к файлу, где будут храниться метаданные.
+        """
+        self.metadata_file = metadata_file
+
+
+    def metadata_exists(self) -> bool:
+        """
+        Проверяет существование файла метаданных.
+
+        :return: True, если файл метаданных существует, иначе False.
+        """
+        return os.path.exists(self.metadata_file)
+
+    def clean_metadata(self):
+        """
+        Очищает файл метаданных, если он существует.
+        """
+        if self.metadata_exists():
+            try:
+                os.remove(self.metadata_file)
+                logging.info(f"Файл метаданных {self.metadata_file} очищен.")
+            except Exception as e:
+                logging.error(f"Ошибка при очистке файла метаданных {self.metadata_file}: {e}")
+        else:
+            logging.info(f"Файл метаданных {self.metadata_file} не существует. Очистка не требуется.")
+    def save_metadata(self, metadata: dict) -> None:
+        """
+        Сохраняет метаданные в файл.
+
+        :param metadata: Словарь метаданных для сохранения.
+        """
+        try:
+            with open(self.metadata_file, "w", encoding="utf-8") as file:
+                json.dump(metadata, file, indent=4, ensure_ascii=False)
+            logging.info(f"Метаданные успешно сохранены в {self.metadata_file}.")
+        except Exception as e:
+            logging.error(f"Ошибка при сохранении метаданных в {self.metadata_file}: {e}")
+
+    def load_metadata(self) -> dict:
+        """
+        Загружает метаданные из файла.
+
+        :return: Словарь метаданных.
+        """
+        if not os.path.exists(self.metadata_file):
+            logging.error(f"Файл метаданных {self.metadata_file} не найден.")
+            return {}
+
+        try:
+            with open(self.metadata_file, "r", encoding="utf-8") as file:
+                metadata = json.load(file)
+                logging.info(f"Метаданные успешно загружены из {self.metadata_file}.")
+                return metadata
+        except Exception as e:
+            logging.error(f"Ошибка при загрузке метаданных из {self.metadata_file}: {e}")
+            return {}
+
+
