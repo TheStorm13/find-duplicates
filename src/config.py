ed9afb6aa2a8e311b773020192cadd454ac97d62
ilya_groza <ilya.groza13@gmail.com>
2025-04-11 15:12:41 +0400

    fix: update log file path to reflect new directory structure

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ import os
 # Автоматически определяем корень проекта
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-LOG_FILE_PATH = os.path.join(PROJECT_ROOT, 'src', 'logs', 'app.log')
+LOG_FILE_PATH = os.path.join(PROJECT_ROOT, 'logs', 'app.log')
 DUPLICATE_FOLDER = "Duplicate"
 METADATA_FILE_NAME = "duplicates_metadata.json"
 IMAGE_EXTENSIONS = [".jpg", "jpeg", ".png"]
